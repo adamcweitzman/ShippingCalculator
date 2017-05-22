6f1585b21e9cd61a11a31b96b55b2a5a3fbc81fe
Adam Weitzman <Adam.Weitzman@sync1systems.com>
2017-05-22 15:15:59 -0500

    in the beggining

---
+++ ShippingCalculator/wwwroot/js/site.js
@@ -0,0 +1,44 @@
+﻿// Write your Javascript code.
+$(function () {
+
+
+
+    console.log("I'm here");
+
+    $(".radio").click(function () {
+        console.log("i hit the readio btn");
+        var selVal = $('input[name=optradio]:checked').val(); 
+        $('#labelMass').html('Shipping Mass Per Case (' + selVal + ')');
+    })
+
+
+    $("#calcBtn").click(function (e) {
+        e.preventDefault();
+        console.log("i hit the calc button");
+        var mass = $("#shippingMassPerCase").val();
+        var cases = $('#numberCases').val();
+
+        var selVal = $('input[name=optradio]:checked').val(); 
+        if (selVal == "g") {
+            var sum = ((mass / 1000) * 5) * cases
+        }
+        else {
+            var sum = (mass * 5) * cases
+        }
+        
+
+        console.log(sum);
+
+        $('#sum').html(sum);
+        
+    })
+
+
+
+
+
+
+});
+
+
+
